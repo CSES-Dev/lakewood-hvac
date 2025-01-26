d96c2cc8b5a873bf69a54c61ede2035ac821dfd8
kendrick010 <nguyen.kendrick@yahoo.com>
2025-01-26 12:06:15 -0800

    div height

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,10 +4,10 @@ export default function ContactPage() {
     return (
         <div className="flex flex-col items-center bg-[#2C2C2C] min-h-screen text-white px-4">
             <div className="w-full max-w-7xl">
-                <h1 className="text-8xl font-medium mb-20">Contact Us</h1>
+                <h1 className="text-7xl font-medium mb-20">Contact Us</h1>
             </div>
 
-            <div className="flex flex-col md:flex-row gap-12 w-full max-w-7xl h-screen">
+            <div className="flex flex-col md:flex-row gap-12 w-full max-w-7xl">
                 {/* Contact Form */}
                 <div className="bg-[#A8CCA0BF] p-10 rounded-3xl flex-1">
                     <form className="flex flex-col gap-4">
@@ -44,7 +44,7 @@ export default function ContactPage() {
                         <textarea
                             name="message"
                             placeholder="How can we help?"
-                            className="p-3 pl-6 rounded-2xl bg-[#FFFDF6] h-56 text-input text-2xl font-light placeholder-input"
+                            className="p-3 pl-6 rounded-2xl bg-[#FFFDF6] h-64 text-input text-2xl font-light placeholder-input"
                             required
                         />
 
@@ -58,7 +58,7 @@ export default function ContactPage() {
                 </div>
 
                 {/* Google Map */}
-                <div className="rounded-2xl flex-1 bg-white h-3/4">
+                <div className="rounded-2xl flex-1 bg-white h-[75vh]">
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3313.4026836440325!2d-118.13527642424282!3d33.85351167323249!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dd328b9d836d55%3A0xd4badfa32c2ad99b!2sLakewood%20Heating%20and%20Air%20Conditioning%20Inc.!5e0!3m2!1sen!2sus!4v1737916289075!5m2!1sen!2sus"
                         width="100%"
