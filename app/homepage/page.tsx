11255ed75f989823ffc82e5bb69c75b86e7f74ad
jag039 <jag039@ucsd.edu>
2025-02-03 16:38:20 -0800

    Added work examples onto HomePage, and fixed image sizing issue and ensured consistent layout

--- app/homepage/page.tsx
+++ app/homepage/page.tsx
@@ -11,7 +11,54 @@ export default function Page() {
             {/* Services */}
             <div>
                 {/* Squares - Jesus*/}
-                <div></div>
+                <div className="px-4 py-8 sm:p-8 mx-auto">
+                    <h2 className="text-4xl text-center font-bold mb-8 text-primary-foreground">
+                        Professional Work, With Professional Services
+                    </h2>
+                    {/* Images */}
+                    <div className="flex flex-wrap lg:flex-nowrap w-full p-4 gap-4 justify-center">
+                        <div className="max-w-xs relative rounded-lg overflow-hidden">
+                            <img
+                                src="/homePage/AC.png"
+                                alt="Air Conditioning"
+                                className="w-full h-full object-cover"
+                            />
+                            <div className="absolute bottom-0 w-full bg-white/80 text-black text-sm font-semibold p-2">
+                                Air Conditioning
+                            </div>
+                        </div>
+                        <div className="max-w-xs relative rounded-lg overflow-hidden">
+                            <img
+                                src="/homePage/Heating.png"
+                                alt="Air Conditioning"
+                                className="w-full h-full object-cover"
+                            />
+                            <div className="absolute bottom-0 w-full bg-white/80 text-black text-sm font-semibold p-2">
+                                Heating
+                            </div>
+                        </div>
+                        <div className="max-w-xs relative rounded-lg overflow-hidden">
+                            <img
+                                src="/homePage/Thermostats.png"
+                                alt="Air Conditioning"
+                                className="w-full h-full object-cover"
+                            />
+                            <div className="absolute bottom-0 w-full bg-white/80 text-black text-sm font-semibold p-2">
+                                Thermostats
+                            </div>
+                        </div>
+                        <div className="max-w-xs relative rounded-lg overflow-hidden">
+                            <img
+                                src="/homePage/HeatPumps.png"
+                                alt="Air Conditioning"
+                                className="w-full h-full object-cover"
+                            />
+                            <div className="absolute bottom-0 w-full bg-white/80 text-black text-sm font-semibold p-2">
+                                HeatPumps
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 {/* Circles - Brandon*/}
                 <div></div>
             </div>
