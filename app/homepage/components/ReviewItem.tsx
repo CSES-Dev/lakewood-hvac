48063153030449dffebc99822c668c2214b91838
jag039 <jag039@ucsd.edu>
2025-02-04 21:52:54 -0800

    Component-ized review item

---
+++ app/homepage/components/ReviewItem.tsx
@@ -0,0 +1,15 @@
+const ReviewItem = ({ text, author, animation }: { text: string, author: string, animation: string }) => {
+    return (
+        <div
+        className={`flex flex-col m-4 p-4 lg:w-full bg-primary-foreground text-black rounded-lg min-h-40 shadow-md transform ${animation} transition-all duration-300`}
+        >
+            <div className="flex-grow overflow-hidden text-ellipsis line-clamp-4">
+                {text}
+            </div>
+            <hr className="my-2" />
+            <div className="font-semibold">{author}</div>
+        </div>
+    );
+};
+
+export default ReviewItem;
