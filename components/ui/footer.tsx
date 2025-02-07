4e9291377848f145e3de37e1c39a1d5fd3fc6eac
Kyla <kyla123ma@gmail.com>
2025-02-06 16:50:55 -0800

    fixed mobile footer

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const Footer: React.FC = () => {
     return (
-        <footer className="bg-[#4F6E4E] text-[#F0F0F0] py-8 border-t border-border w-full text-[min(2.5vw, 24px)] font-sans px-[4vw] mt-auto overflow-hidden">
-            <div className="max-w-screen-lg mx-auto flex items-center justify-between w-full">
+        <footer className="bg-[#4F6E4E] text-[#F0F0F0] py-8 border-t border-[#4F6E4E] w-full h-auto min-h-[30vh] font-sans px-[4vw] mt-auto overflow-hidden flex flex-col sm:flex-row items-center sm:h-[30vh]">
+            <div className="max-w-screen-lg mx-auto flex flex-col sm:flex-row items-center justify-center w-full h-full">
                 <Image
                     src="/lakewoodlogo.png"
                     alt="Lakewood Icon"
@@ -12,10 +12,10 @@ const Footer: React.FC = () => {
                     width={266}
                     height={149}
                     style={{ flexShrink: 0 }}
-                    className="w-[30vw] h-auto"
+                    className="w-[500vw] md:w-[200vw] xs:w-[300px] lg:w-[300px] max-h-[20vh] h-auto object-contain"
                 />
-                <div className="w-1/2 flex flex-col justify-center min-w-0">
-                    <p className="mb-[1vw] flex items-center gap-[2vw]">
+                <div className="w-full sm:w-1/2 flex flex-col items-center sm:items-start text-center sm:text-left mt-4 sm:mt-0">
+                    <p className="mb-[0.8vw] flex items-center gap-[1.2vw] text-xs sm:text-xs md:text-base lg:text-lg">
                         <Image
                             src="/call.png"
                             alt="Call Icon"
@@ -23,11 +23,11 @@ const Footer: React.FC = () => {
                             width={29}
                             height={29}
                             style={{ flexShrink: 0 }}
-                            className="w-[3vw] h-auto"
+                            className="w-[2vw] h-auto"
                         />
                         <a href={`tel:5626336412`}>562-633-6412</a>
                     </p>
-                    <p className="mb-[1vw] flex items-center gap-[2vw]">
+                    <p className="mb-[0.8vw] flex items-center gap-[1.2vw] text-xs sm:text-xs md:text-base lg:text-lg justify-center">
                         <Image
                             src="/mail.png"
                             alt="Mail Icon"
@@ -35,13 +35,13 @@ const Footer: React.FC = () => {
                             width={29}
                             height={29}
                             style={{ flexShrink: 0 }}
-                            className="w-[3vw] h-auto"
+                            className="w-[2vw] h-auto"
                         />
                         <a href={`mailto:lakewooodhvac90712@gmail.com`}>
                             lakewooodhvac90712@gmail.com
                         </a>
                     </p>
-                    <p className="mb-[1vw] flex items-center gap-[2vw]">
+                    <p className="mb-[0.8vw] flex items-center gap-[1.2vw] text-xs sm:text-xs md:text-base lg:text-lg justify-center">
                         <Image
                             src="/clock.png"
                             alt="Clock Icon"
@@ -49,7 +49,7 @@ const Footer: React.FC = () => {
                             width={29}
                             height={29}
                             style={{ flexShrink: 0 }}
-                            className="w-[3vw] h-auto"
+                            className="w-[2vw] h-auto"
                         />
                         <a>Mondays-Saturdays: 8AM-8PM</a>
                     </p>
@@ -58,4 +58,5 @@ const Footer: React.FC = () => {
         </footer>
     );
 };
+
 export default Footer;
