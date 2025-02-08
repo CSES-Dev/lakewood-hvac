68ade39b91281a5e094275e72235cae3882552cc
Kyla <kyla123ma@gmail.com>
2025-02-07 16:56:26 -0800

    Deleted footer from sample page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
-import Footer from "@/components/ui/footer";
-
 export default function Home() {
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen  font-[family-name:var(--font-geist-sans)] w-full">
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
                 <div className="flex gap-4 items-center flex-col sm:flex-row"></div>
             </main>
-
-            <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center w-full">
-                <Footer />
-            </footer>
         </div>
     );
 }
