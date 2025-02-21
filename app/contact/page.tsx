f953b645699e8e6d26b6d2f748979dd2129299c7
kendrick010 <nguyen.kendrick@yahoo.com>
2025-02-20 23:28:02 -0800

    form input width

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,29 +1,19 @@
-import React from "react";
 import ContactForm from "./ContactForm";
-import NavigationBar from "./NavigationBar"
-import ContactInfo from "./ContactInfo";
-import ContactComponent from "./ContactComponent";
-
 
 export default function ContactPage() {
     return (
         <div className="flex overflow-hidden flex-col bg-background">
-            <main className="flex flex-col px-16 w-full max-md:px-5 max-md:my-10 max-md:max-w-full">
-                <NavigationBar />
-                <br />
-                <br />
-                <br />
-                <h1 className="flex flex-col my-16 w-full text-5xl font-medium leading-none max-w-[1120px] text-header max-md:max-w-full max-md:text-5xl">
+            <main className="flex flex-col px-16 w-full mb-16 max-md:px-5 max-md:max-w-full">
+                <h1 className="flex flex-col my-16 w-full text-8xl font-medium leading-none text-header max-md:my-6 max-md:text-6xl">
                     Contact Us
-                    <div className="flex max-w-full max-md:min-h-[5px]" />
                 </h1>
 
                 <div className="z-10 w-full max-md:max-w-full">
                     <div className="flex gap-5 max-md:flex-col">
-                        <div className="flex flex-col w-6/12 max-md:ml-0 max-md:w-full">
+                        <div className="flex flex-col w-7/12 max-md:ml-0 max-md:w-full">
                             <ContactForm />
                         </div>
-                        <div className="flex flex-col ml-5 w-6/12 max-md:ml-0 max-md:w-full">
+                        <div className="flex flex-col ml-8 w-5/12 max-md:ml-0 max-md:w-full">
                             <div className="flex overflow-hidden flex-col w-full rounded-3xl max-md:mt-10 max-md:max-w-full">
                                 <iframe
                                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3313.4026836440325!2d-118.13527642424282!3d33.85351167323249!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dd328b9d836d55%3A0xd4badfa32c2ad99b!2sLakewood%20Heating%20and%20Air%20Conditioning%20Inc.!5e0!3m2!1sen!2sus!4v1737916289075!5m2!1sen!2sus"
@@ -38,10 +28,6 @@ export default function ContactPage() {
                     </div>
                 </div>
             </main>
-
-            <div className="mt-12">
-                <ContactComponent />
-            </div>
         </div>
     );
 }
