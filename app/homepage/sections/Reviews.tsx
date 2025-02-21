938b9b1eccf900e1390b008a82cbf92a4d31b1f9
binkybarnes <cottonjohn2000@gmail.com>
2025-02-20 18:06:14 -0800

    headings alignments

--- app/homepage/sections/Reviews.tsx
+++ app/homepage/sections/Reviews.tsx
@@ -74,7 +74,7 @@ export default function Reviews() {
 
     return (
         <div className="px-4 py-8 sm:p-8 mx-auto overflow-y-hidden">
-            <h2 className="text-5xl text-left font-bold mb-8 text-primary-foreground">
+            <h2 className="text-5xl text-left font-bold mb-8 text-primary-foreground pl-[3.5rem] lg:pl-[4.5rem]">
                 See What Our Clients Say
             </h2>
 
