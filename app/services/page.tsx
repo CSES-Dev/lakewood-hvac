443931d2d8d611a2b427fbd83538d63180058917
angelawwu236 <zhu740513@gmail.com>
2025-02-24 18:32:17 -0800

    made services page mobile responsive

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -6,26 +6,28 @@ export default function Services() {
         <main className="bg-background px-[6.11vw] pt-[5.28vw] pb-[5vw] p-[0.69vw]">
             <div className="pb-[5vw]">
                 <div className="text-[#F0F0F0] font-sans">
-                    <h1 className="text-[6.25vw] ">Services</h1>
-                    <p className="text-[2.22vw]">
+                    <h1 className="text-[6.25vw] max-sm:text-[12.5vw]">Services</h1>
+                    <p className="text-[2.22vw] max-sm:text-[4.44vw]">
                         We offer a variety of services, from air conditioning to thermostats.
                     </p>
                 </div>
             </div>
             <div>
                 <div className="text-[#F0F0F0] font-sans flex max-lg:flex-col w-full max-md:items-center max-sm:items-center max-container gap-[3.4vw]">
-                    <div className="w-[33.33] flex-none flex rounded-[1.74vw]">
+                    <div className="max-sm:w-full md:w-[33.33] flex-none flex rounded-[1.74vw]">
                         <Image
                             src="/ac.png"
                             alt="AC Image"
                             width={424}
                             height={424}
-                            className="w-[29vw] h-[29vw]"
+                            className="w-[29vw] h-[29vw] max-sm:w-full max-sm:h-auto"
                         />
                     </div>
                     <div className="flex w-[66.67] flex-1 flex-col">
-                        <p className=" pb-[1.67vw] text-[4.51vw]">Air Conditioning</p>
-                        <p className=" pb-[2.22vw] text-[1.67vw]">
+                        <p className=" pb-[1.67vw] text-[4.51vw] max-sm:text-[9.02vw]">
+                            Air Conditioning
+                        </p>
+                        <p className=" pb-[2.22vw] text-[1.67vw] max-sm:text-[3.34vw]">
                             At Lakewood Heating and Air Conditioning Inc., we pride ourselves on
                             providing top-notch air conditioning services to ensure that our clients
                             enjoy a comfortable living environment. Our team of skilled technicians
@@ -36,25 +38,25 @@ export default function Services() {
                             comfortable throughout the year.
                         </p>
                         <div>
-                            <button className="text-[1.74vw] bg-primary rounded-[1.39vw] py-[1.04vw] px-[1.94vw]">
+                            <button className="text-[1.74vw] bg-primary rounded-[1.39vw] py-[1.04vw] px-[1.94vw] max-sm:text-[3.48vw]">
                                 Schedule Today
                             </button>
                         </div>
                     </div>
                 </div>
                 <div className="py-[5vw] text-[#F0F0F0] font-sans flex max-lg:flex-col w-full max-md:items-center max-sm:items-center max-container gap-[3.4vw]">
-                    <div className="w-[33.33] flex-none flex rounded-[1.74vw]">
+                    <div className="max-sm:w-full w-[33.33] flex-none flex rounded-[1.74vw]">
                         <Image
                             src="/heating.png"
                             alt="Heating Image"
                             width={424}
                             height={424}
-                            className="w-[29vw] h-[29vw]"
+                            className="w-[29vw] h-[29vw]  max-sm:w-full max-sm:h-auto"
                         />
                     </div>
                     <div className="flex w-[66.67] flex-1 flex-col">
-                        <p className=" pb-[1.67vw] text-[4.51vw]">Heating</p>
-                        <p className=" pb-[2.22vw] text-[1.67vw]">
+                        <p className=" pb-[1.67vw] text-[4.51vw] max-sm:text-[9.02vw]">Heating</p>
+                        <p className=" pb-[2.22vw] text-[1.67vw] max-sm:text-[3.34vw]">
                             Lakewood Heating and Air Conditioning Inc. is your reliable partner for
                             all your heating needs. Our experienced technicians are committed to
                             delivering exceptional heating services, from installations to repairs
@@ -64,25 +66,27 @@ export default function Services() {
                             superior customer satisfaction. Give us a call today to learn more!
                         </p>
                         <div>
-                            <button className="text-[1.74vw] bg-primary rounded-[1.39vw] py-[1.04vw] px-[1.94vw]">
+                            <button className="text-[1.74vw] max-sm:text-[3.48vw] bg-primary rounded-[1.39vw] py-[1.04vw] px-[1.94vw]">
                                 Schedule Today
                             </button>
                         </div>
                     </div>
                 </div>
                 <div className="py-[5vw] text-[#F0F0F0] font-sans flex max-lg:flex-col w-full max-md:items-center max-sm:items-center max-container gap-[3.4vw]">
-                    <div className="w-[33.33] flex-none flex rounded-[1.74vw]">
+                    <div className="max-sm:w-full w-[33.33] flex-none flex rounded-[1.74vw]">
                         <Image
                             src="/thermostats.png"
                             alt="Thermostats Image"
                             width={424}
                             height={424}
-                            className="w-[29vw] h-[29vw]"
+                            className="w-[29vw] h-[29vw]  max-sm:w-full max-sm:h-auto"
                         />
                     </div>
                     <div className="flex w-[66.67] flex-1 flex-col">
-                        <p className=" pb-[1.67vw] text-[4.51vw]">Thermostats</p>
-                        <p className=" pb-[2.22vw] text-[1.67vw]">
+                        <p className=" pb-[1.67vw] text-[4.51vw] max-sm:text-[9.02vw]">
+                            Thermostats
+                        </p>
+                        <p className=" pb-[2.22vw] text-[1.67vw] max-sm:text-[3.34vw]">
                             Our thermostat services include professional installation, maintenance,
                             and repair of a variety of thermostat types and models. Our experts work
                             with you to identify the ideal thermostat solution for your home,
@@ -91,25 +95,27 @@ export default function Services() {
                             to get started!
                         </p>
                         <div>
-                            <button className="text-[1.74vw] bg-primary rounded-[1.39vw] py-[1.04vw] px-[1.94vw]">
+                            <button className="text-[1.74vw] max-sm:text-[3.48vw] bg-primary rounded-[1.39vw] py-[1.04vw] px-[1.94vw]">
                                 Schedule Today
                             </button>
                         </div>
                     </div>
                 </div>
                 <div className="py-[5vw] text-[#F0F0F0] font-sans flex max-lg:flex-col w-full max-md:items-center max-sm:items-center max-container gap-[3.4vw]">
-                    <div className="w-[33.33] flex-none flex rounded-[1.74vw]">
+                    <div className="max-sm:w-full w-[33.33] flex-none flex rounded-[1.74vw]">
                         <Image
                             src="/heat_pumps.png"
                             alt="Heat Pumps Image"
                             width={424}
                             height={424}
-                            className="w-[29vw] h-[29vw]"
+                            className="w-[29vw] h-[29vw]  max-sm:w-full max-sm:h-auto"
                         />
                     </div>
                     <div className="flex w-[66.67] flex-1 flex-col">
-                        <p className=" pb-[1.67vw] text-[4.51vw]">Heat Pumps</p>
-                        <p className=" pb-[2.22vw] text-[1.67vw]">
+                        <p className=" pb-[1.67vw] text-[4.51vw] max-sm:text-[9.02vw] ">
+                            Heat Pumps
+                        </p>
+                        <p className=" pb-[2.22vw] text-[1.67vw] max-sm:text-[3.34vw]">
                             Lakewood Heating and Air Conditioning Inc. offers comprehensive heat
                             pump installation services, designed to provide you with an
                             eco-friendly, energy-efficient, and cost-effective alternative to
@@ -120,7 +126,7 @@ export default function Services() {
                             reducing your energy consumption and costs.
                         </p>
                         <div>
-                            <button className="text-[1.74vw] bg-primary rounded-[1.39vw] py-[1.04vw] px-[1.94vw]">
+                            <button className="text-[1.74vw] max-sm:text-[3.48vw] bg-primary rounded-[1.39vw] py-[1.04vw] px-[1.94vw]">
                                 Schedule Today
                             </button>
                         </div>
