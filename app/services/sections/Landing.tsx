0ebab265587b5dee9c578cc8ee435cf00f269a88
angelawwu236 <zhu740513@gmail.com>
2025-02-27 14:57:58 -0800

    fixed medium screen and finalized services page

---
+++ app/services/sections/Landing.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Landing() {
+    return (
+        <div className="pb-[5vw]">
+            <div className="text-[#F0F0F0] font-sans">
+                <h1 className="text-[6.25vw] max-sm:text-[12.5vw] max-lg:text-[9.38vw]">
+                    Services
+                </h1>
+                <p className="text-[2.22vw] max-sm:text-[4.44vw] max-lg:text-[3.33vw]">
+                    We offer a variety of services, from air conditioning to thermostats.
+                </p>
+            </div>
+        </div>
+    );
+}
---
+++ app/services/sections/ServiceSection.tsx
@@ -0,0 +1,55 @@
+import ServiceItem from "../components/ServiceItem";
+
+const ServiceSection = () => {
+    return (
+        <div>
+            <ServiceItem
+                title="Air Conditioning"
+                description="At Lakewood Heating and Air Conditioning Inc., we pride ourselves on
+                            providing top-notch air conditioning services to ensure that our clients
+                            enjoy a comfortable living environment. Our team of skilled technicians
+                            is well-equipped to handle installations, maintenance, and repairs for a
+                            variety of air conditioning systems. No matter the size or complexity of
+                            your cooling needs, you can trust us to deliver efficient,
+                            cost-effective, and timely solutions that keep your home cool and
+                            comfortable throughout the year."
+                source="/ac.png"
+            />
+            <ServiceItem
+                title="Heating"
+                description="Lakewood Heating and Air Conditioning Inc. is your reliable partner for
+                            all your heating needs. Our experienced technicians are committed to
+                            delivering exceptional heating services, from installations to repairs
+                            and maintenance for a wide range of heating systems. Our goal is to
+                            provide you with energy-efficient, safe, and comfortable heating
+                            solutions to keep your home cozy during the colder months while ensuring
+                            superior customer satisfaction. Give us a call today to learn more!"
+                source="/heating.png"
+            />
+            <ServiceItem
+                title="Thermostats"
+                description="Our thermostat services include professional installation, maintenance,
+                            and repair of a variety of thermostat types and models. Our experts work
+                            with you to identify the ideal thermostat solution for your home,
+                            ensuring optimal temperature regulation and energy management,
+                            ultimately enhancing your comfort and reducing energy costs. Contact us
+                            to get started!"
+                source="/thermostats.png"
+            />
+            <ServiceItem
+                title="Heat Pumps"
+                description="Lakewood Heating and Air Conditioning Inc. offers comprehensive heat
+                            pump installation services, designed to provide you with an
+                            eco-friendly, energy-efficient, and cost-effective alternative to
+                            traditional heating and cooling systems. Our skilled technicians are
+                            proficient in installing a wide range of heat pump models, ensuring
+                            seamless integration into your existing HVAC system. Trust us to help
+                            you transition to a greener, more comfortable living space while
+                            reducing your energy consumption and costs."
+                source="/heat_pumps.png"
+            />
+        </div>
+    );
+};
+
+export default ServiceSection;
