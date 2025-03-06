aac124ea6bfa85b1f68ed48656e95296a023c299
angelawwu236 <zhu740513@gmail.com>
2025-03-06 15:35:12 -0800

    adjusted size for large screens

--- app/services/sections/Landing.tsx
+++ app/services/sections/Landing.tsx
@@ -4,10 +4,10 @@ export default function Landing() {
     return (
         <div className="pb-[5vw]">
             <div className="text-[#F0F0F0] font-sans">
-                <h1 className="text-[6.25vw] max-sm:text-[12.5vw] max-lg:text-[9.38vw]">
+                <h1 className="text-[6.25vw] max-sm:text-[12.5vw] max-lg:text-[9.38vw] max-text-[90px]">
                     Services
                 </h1>
-                <p className="text-[2.22vw] max-sm:text-[4.44vw] max-lg:text-[3.33vw]">
+                <p className="text-[2.22vw] max-sm:text-[4.44vw] max-lg:text-[3.33vw] max-text-[32px]">
                     We offer a variety of services, from air conditioning to thermostats.
                 </p>
             </div>
