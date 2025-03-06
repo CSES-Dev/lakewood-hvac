fbf275d50641a8529c8c789c2379d7a74f6b3102
angelawwu236 <zhu740513@gmail.com>
2025-03-06 15:51:06 -0800

    change size for large screen

--- app/services/components/ServiceItem.tsx
+++ app/services/components/ServiceItem.tsx
@@ -17,18 +17,18 @@ const ServiceItem = ({
                     alt={title}
                     width={424}
                     height={424}
-                    className="justify-center w-[29vw] h-[29vw]  max-w-[555px] max-sm:w-full max-lg:w-[43.5vw] max-lg:h-auto"
+                    className="justify-center w-[29vw] h-[29vw]  max-lg:w-[555px] max-sm:w-full max-lg:w-[43.5vw] max-lg:h-auto"
                 />
             </div>
             <div className="flex w-[66.67] flex-1 flex-col">
-                <p className=" pb-[1.67vw] text-[4.51vw] max-sm:text-[9.02vw] max-text-[65px] ax-lg:text-[6.77vw] leading-none">
+                <p className=" pb-[1.67vw] text-[4.51vw] max-sm:text-[9.02vw] max-lg:text-[65px] max-lg:text-[6.77vw] leading-none">
                     {title}
                 </p>
-                <p className=" pb-[2.22vw] text-[1.67vw] max-sm:text-[3.34vw]  max-text-[24px] max-lg:text-[2.51vw]">
+                <p className=" pb-[2.22vw] text-[1.67vw] max-sm:text-[3.34vw]  max-lg:text-[24px] max-lg:text-[2.51vw]">
                     {description}
                 </p>
                 <div>
-                    <button className="text-[1.74vw] bg-primary rounded-[1.39vw] max-text-[25px] py-[1.04vw] px-[1.94vw] max-sm:text-[3.48vw] max-lg:text-[2.61vw]">
+                    <button className="text-[1.74vw] bg-primary rounded-[1.39vw] max-lg:text-[25px] py-[1.04vw] px-[1.94vw] max-sm:text-[3.48vw] max-lg:text-[2.61vw]">
                         Schedule Today
                     </button>
                 </div>
