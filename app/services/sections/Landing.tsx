88606f931e8e7a3446a07efced4f54027b5638dd
angelawu236 <zhu740513@gmail.com>
2025-04-22 16:01:27 -0700

    fixed ui

--- app/services/sections/Landing.tsx
+++ app/services/sections/Landing.tsx
@@ -4,9 +4,7 @@ export default function Landing() {
     return (
         <div className="pb-[5vw]">
             <div className="text-[#F0F0F0] font-sans">
-                <h1 className="max-sm:text-[12.5vw] text-[clamp(0px,6.25vw,135px)]">
-                    Services
-                </h1>
+                <h1 className="max-sm:text-[12.5vw] text-[clamp(0px,6.25vw,135px)]">Services</h1>
                 <p className="text-[2.22vw] max-sm:text-[4.44vw] text-[clamp(0px,2.22vw,48px)]">
                     We offer a variety of services, from air conditioning to thermostats.
                 </p>
