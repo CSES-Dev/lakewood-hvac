5e1a6feff772614197af0621875a8b5e69b2bead
kendrick010 <nguyen.kendrick@yahoo.com>
2025-05-11 21:52:31 -0700

    rebase for file upload

--- app/adminpanel/components/EngagementTable/EngagementRow.tsx
+++ app/adminpanel/components/EngagementTable/EngagementRow.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import { FaImage } from "react-icons/fa6";
 import { Engagement } from "@/models/Engagement";
 
 type EnagementRowProps = {
@@ -9,13 +10,45 @@ type EnagementRowProps = {
     onDelete: (id: number) => void;
 };
 
-export default function ReviewRow({ engagement, onEdit, onDelete }: EnagementRowProps) {
+export default function EngagementRow({ engagement, onEdit, onDelete }: EnagementRowProps) {
     return (
         <tr className="border">
+            <td className="p-2 text-center align-middle">
+                <input
+                    type="checkbox"
+                    checked={engagement.isVisible}
+                    disabled
+                    className="mx-auto scale-125"
+                />
+            </td>
             <td className="p-2">{engagement.title}</td>
-            <td className="p-2">{engagement.date.toString()}</td>
+            <td className="p-2">
+                {new Date(engagement.date)
+                    .toLocaleString("en-US", {
+                        weekday: "long",
+                        month: "long",
+                        day: "numeric",
+                        hour: "numeric",
+                        year: "numeric",
+                        minute: "2-digit",
+                        hour12: true,
+                    })
+                    .replace("at", "•")}
+            </td>
             <td className="p-2">{engagement.description}</td>
-            <td className="p-2">{engagement.isVisible.toString()}</td>
+            <td className="p-2">
+                <div className="flex items-center space-x-2">
+                    <FaImage className="text-gray-500" />
+                    <a
+                        href={engagement.imageUrl}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-blue-600 underline hover:text-blue-800"
+                    >
+                        {engagement.imageUrl}
+                    </a>
+                </div>
+            </td>
             <td className="p-2 text-right">
                 <div className="flex flex-col items-end space-y-1">
                     <button
