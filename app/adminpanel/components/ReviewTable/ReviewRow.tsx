40e09c95f8d7f52087f437c800c5897064bc30d1
kendrick010 <nguyen.kendrick@yahoo.com>
2025-05-12 00:29:45 -0700

    fixed admin panel page review type error
    fixed merge conflicts

--- app/adminpanel/components/ReviewTable/ReviewRow.tsx
+++ app/adminpanel/components/ReviewTable/ReviewRow.tsx
@@ -11,9 +11,9 @@ type ReviewRowProps = {
 
 export default function ReviewRow({ review, onEdit, onDelete }: ReviewRowProps) {
     return (
-        <TemplateRow<Review> item={review} onEdit={onEdit} onDelete={onDelete}>
-            <td className="p-4">{review.author}</td>
-            <td className="p-4">
+        <tr className="border">
+            <td className="p-2">{review.author}</td>
+            <td className="p-2">
                 {new Date(review.createdAt)
                     .toLocaleString("en-US", {
                         weekday: "long",
@@ -26,8 +26,28 @@ export default function ReviewRow({ review, onEdit, onDelete }: ReviewRowProps) 
                     })
                     .replace(/\bat\b/, "•")}
             </td>
+            <td className="p-2">{review.comments}</td>
+            <td className="p-2">{review.rating}</td>
+            <td className="p-2 text-right">
+                <button
+                    className="bg-yellow-500 text-white px-3 py-1 rounded mb-1 w-20"
+                    onClick={() => {
+                        onEdit(review);
+                    }}
+                >
+                    Edit
+                </button>
+                <button
+                    className="bg-red-500 text-white px-3 py-1 rounded w-20"
+                    onClick={() => {
+                        onDelete(review.id);
+                    }}
+                >
+                    Delete
+                </button>
+            </td>
             <td className="p-4">{review.comments}</td>
             <td className="p-4 text-center">{review.rating}</td>
-        </TemplateRow>
+        </tr>
     );
 }
